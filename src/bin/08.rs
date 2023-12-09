8c4fbe85d3fd6cfdcea3809655d3ac268a93353c
Daniel Vorhauer <daniel@hexerei.net>
2023-12-09 14:44:56 +0100

    day 8 LCM beates prime factorization

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,63 +1,94 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 
 advent_of_code::solution!(8);
 
 fn parse_game(input: &str) -> (Vec<usize>, BTreeMap<String, (String, String)>) {
-    // seperate path and tree
+
     let (path, tree) = input.split_once("\n\n").unwrap();
+
     // parse path into list of usize (0 = left, 1 = right)
     let path = path.chars().filter_map(|c| match c {
             'L' => Some(0), 'R' => Some(1), _ => None,
         }).collect::<Vec<usize>>();
-    // parse binary tree with entries like AAA = (BBB, CCC)
-    //let lookup: HashMap<String, usize> = HashMap::new();
-    //let index: usize = 0;
+
+        // parse binary tree with entries like AAA = (BBB, CCC)
     let tree = tree.split(")\n").filter_map(|line| {
         if line.is_empty() {
             return None;
         }
         let (parent, children) = line.split_once(" = (").unwrap();
-        //lookup.insert(parent.to_string(), 0);
         let (left, right) = children.trim().split_once(", ").unwrap();
         Some((parent.to_string(), (left.to_string(), right.to_string())))
     }).collect::<BTreeMap<_,_>>();
 
-    // let parents = tree.iter().enumerate()
-    //     .map(|(i, (parent, _))| (parent.to_string(), i))
-    //     .collect::<HashMap<_,_>>();
-    // let tree = tree.iter().map(|(parent, (left, right))| {
-    //     (parents[parent.to_string()], (parents[left.to_string()], parents[right.to_string()]))
-    // }).collect::<BTreeMap<_,_>>();
-
-    dbg!(&tree);
     (path, tree)
 }
 
+fn solve_path(start: &str, end: &str, path: &Vec<usize>, tree: &BTreeMap<String, (String, String)>) -> usize {
+    let mut current = tree.get(start).unwrap();
+    let mut count: usize = 1;
+    loop {
+        for step in path.iter() {
+            let next_key = match step {
+                0 => &current.0,
+                1 => &current.1,
+                _ => panic!("invalid step"),
+            };
+            if next_key.ends_with(end) {
+                return count;
+            }
+            count += 1;
+            current = tree.get(next_key).unwrap();
+        }
+    }
+}
+
+// calculate greatest common divisor
+fn gcd(mut a:usize, mut b:usize) -> usize{
+    if a==b { return a; }
+    if b > a { std::mem::swap(&mut a, &mut b); }
+    while b>0 {
+        let temp = a;
+        a = b;
+        b = temp%b;
+    }
+    return a;
+}
+
+// calculate least common multiple
+fn lcm(a:usize, b:usize) -> usize{
+    return a*(b/gcd(a,b));
+}
+
+// NOT USED: as LCM is 5 to 30 times faster
+// NOTE: this is a naive implementation of prime factorization
+// fn get_prime(n:usize) -> usize {
+//     let mut i = 2;
+//     while i*i<=n {
+//         if n%i == 0 { return i; }
+//         i+=1;
+//     }
+//     return 0;
+// }
+
 pub fn part_one(input: &str) -> Option<u32> {
     let (path, tree) = parse_game(input);
-
-    let mut current = tree.get("AAA").unwrap();
-    let mut count: usize = 1;
-    for step in path {
-        let next_key = match step {
-            0 => &current.0,
-            1 => &current.1,
-            _ => panic!("invalid step"),
-        };
-        if next_key == "ZZZ" {
-            println!("found ZZZ in {} steps", count);
-            break;
-        }
-        count += 1;
-        current = tree.get(next_key).unwrap();
-    }
-
-    dbg!(current);
-    Some(count as u32)
+    Some(solve_path("AAA", "ZZZ", &path, &tree) as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (path, tree) = parse_game(input);
+    let result = tree.keys()
+        .filter_map(|parent| match parent.ends_with("A") {
+            true => Some(parent.to_string()),
+            false => None,
+        }).collect::<Vec<_>>().iter()
+        .map(|parent| solve_path(parent, "Z", &path, &tree))
+        .collect::<Vec<_>>()
+        .iter().fold(1, |acc, &count| lcm(acc, count));
+    // .iter().fold(1, |acc, &count| acc * get_prime(count)) * path.len();
+    println!("Got {}", result);
+    Some(result as u32)
 }
 
 #[cfg(test)]
@@ -66,13 +97,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_one("LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)");
+        assert_eq!(result, Some(6));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
