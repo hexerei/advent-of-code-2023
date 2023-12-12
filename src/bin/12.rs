63dbaccd75b18e79be65cd4c22861fe600cf4a1a
Daniel Vorhauer <daniel@hexerei.net>
2023-12-12 19:04:15 +0100

    avoided Option

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -3,48 +3,48 @@ use itertools::Itertools;
 
 advent_of_code::solution!(12);
 
-fn permute(row: &[u8], groups: &[usize], is_in: Option<usize>, cache: &mut HashMap<(usize, usize, usize), usize> ) -> usize {
+fn permute(row: &[u8], groups: &[usize], is_in: usize, cache: &mut HashMap<(usize, usize, usize), usize> ) -> usize {
 
     if row.is_empty() {
-        return if (groups.is_empty() && is_in.is_none())
-        || (groups.len() == 1 && is_in.is_some() && is_in.unwrap() == groups[0])
+        return if (groups.is_empty() && is_in == 0)
+        || (groups.len() == 1 && is_in == groups[0])
         { 1 } else { 0 };
     }
 
-    if groups.is_empty() && is_in.is_some() {
+    if groups.is_empty() && is_in > 0 {
         return 0;
     }
 
-    let key = (row.len(), groups.len(), is_in.unwrap_or(0));
+    let key = (row.len(), groups.len(), is_in);
     if let Some(&value) = cache.get(&key) {
         //println!("{:?} = {} CACHE HIT", key, value);
         return value;
     }
 
-    println!("permute {:?} {:?} {:?} {:?}", std::str::from_utf8(row).unwrap(), groups, is_in, key);
+    //println!("permute {:?} {:?} {:?}", std::str::from_utf8(row).unwrap(), groups, key);
     let sum = match row[0] {
-        b'?' => match is_in {
-            Some(n) => {
-                if n == groups[0] {
-                    permute(&row[1..], groups, is_in.map(|n| n+1), cache) +
-                    permute(&row[1..], &groups[1..], None, cache)
-                } else {
-                    permute(&row[1..], groups, is_in.map(|n| n+1), cache)
-                }
-            },
-            None => {
-                permute(&row[1..], groups, Some(1), cache) +
-                permute(&row[1..], groups, None, cache)
-            },
+        b'?' => if is_in > 0 {
+            if is_in == groups[0] {
+                permute(&row[1..], groups, is_in + 1, cache) +
+                permute(&row[1..], &groups[1..], 0, cache)
+            } else {
+                permute(&row[1..], groups, is_in + 1, cache)
+            }
+        } else {
+            permute(&row[1..], groups, 1, cache) +
+            permute(&row[1..], groups, 0, cache)
         },
-        b'#' => match is_in {
-            Some(_) => permute(&row[1..], groups, is_in.map(|n| n+1), cache),
-            None => permute(&row[1..], groups, Some(1), cache),
+        b'#' => if is_in > 0 {
+            permute(&row[1..], groups, is_in + 1, cache)
+        } else {
+            permute(&row[1..], groups, 1, cache)
         },
-        b'.' => match is_in {
-            Some(n) if n != groups[0] => 0,
-            Some(_) => permute(&row[1..], &groups[1..], None, cache),
-            None => permute(&row[1..], groups, None, cache),
+        b'.' => if is_in > 0 {
+            if is_in != groups[0] { 0 } else {
+                permute(&row[1..], &groups[1..], 0, cache)
+            }
+        } else {
+            permute(&row[1..], groups, 0, cache)
         },
         _ => unreachable!(),
     };
@@ -60,10 +60,10 @@ fn solve(row: &str, groups: Vec<usize>, part2: bool) -> usize {
     if part2 {
         let row = (0..5).map(|_| row).join("?");
         let groups = (0..5).flat_map(|_| &groups).copied().collect::<Vec<_>>();
-        result = permute(row.as_bytes(), &groups, None, &mut cache);
+        result = permute(row.as_bytes(), &groups, 0, &mut cache);
         //println!("{} {:?} ... {} permutations", row, groups, result);
     } else {
-        result = permute(row.as_bytes(), &groups, None, &mut cache);
+        result = permute(row.as_bytes(), &groups, 0, &mut cache);
         //println!("{} {:?} ... {} permutations", row, groups, result);
     }
 
