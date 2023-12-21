4f3fa583c6d01e4c96d6eb7df3812be3681ac564
Daniel Vorhauer <daniel@hexerei.net>
2023-12-21 16:10:11 +0100

    part 2 recursive

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,178 +1,99 @@
 use std::collections::HashMap;
-
-use advent_of_code::template::aoc_cli::AocCommandError;
+use itertools::Itertools;
 
 advent_of_code::solution!(19);
 
-fn parse_game(input: &str) -> (HashMap<&str, Vec<(Option<(usize, bool, u32)>,&str)>>, Vec<Vec<u32>>) {
-    let mut flows = HashMap::new();
-    let (workflows, parts) = input.split_once("\n\n").unwrap();
-    workflows.lines().for_each(|line| {
-        let (id, workflow) = line.split_once("{").unwrap();
-        flows.insert( id, workflow.trim_matches('}').split(",").map(|part| {
-            let (rule, goto) = part.split_once(":").unwrap_or(("",part));
-            if rule.is_empty() {
-                (None, goto)
-            } else {
-                let mut chars = rule.chars();
-                let rule = (
-                    match chars.next() {
-                        Some('x') => 0,
-                        Some('m') => 1,
-                        Some('a') => 2,
-                        Some('s') => 3,
-                        _ => unreachable!("{} {:?}", rule, chars)
-                    } as usize,
-                    match chars.next() {
-                        Some('<') => false,
-                        Some('>') => true,
-                        _ => unreachable!("{} {:?}", rule, chars)
-                    },
-                    chars.as_str().parse::<u32>().unwrap()
-                );
-                (Some(rule), goto)
-            }
-        }).collect::<Vec<_>>());
-    });
-    // for (k, v) in flows {
-    //     println!("{:?} -> {:?}", k, v);
-    // }
+const VERBOSE: bool = false;
+type FlowMap<'a> = HashMap<&'a str, (Vec<(usize, bool, u32, &'a str)>, &'a str)>;
+
+fn parse_input(input: &str) -> (FlowMap, Vec<(u32,u32,u32,u32)>) {
+    let (flows, parts) = input.split_once("\n\n").unwrap();
+    let flows = flows.lines().map(|line| {
+        let (id, flow) = line.split_once("{").unwrap();
+        let (flow, end) = flow[0..flow.len()-1].split_at(flow.rfind(',').unwrap());
+        let end = end.trim_matches(',');
+        (id, (flow.split(",").map(|part| {
+            let (rule, goto) = part.split_once(":").unwrap();
+            let mut chars = rule.chars();
+            (
+                match chars.next() {
+                    Some('x') => 0,
+                    Some('m') => 1,
+                    Some('a') => 2,
+                    Some('s') => 3,
+                    _ => unreachable!("{} {:?}", rule, chars)
+                } as usize,
+                match chars.next() {
+                    Some('>') => true,
+                    Some('<') => false,
+                    _ => unreachable!("{} {:?}", rule, chars)
+                },
+                chars.as_str().parse::<u32>().unwrap(),
+                goto
+            )
+        }).collect::<Vec<_>>(), end))
+    }).collect::<HashMap<_,_>>();
     let parts = parts.lines().map(|line| {
         line.trim_matches(['{','}']).split(",").map(|p| {
             p.chars().filter(|c| c.is_numeric()).collect::<String>().parse::<u32>().unwrap()
-        }).collect::<Vec<_>>()
+        }).collect_tuple().unwrap()
     }).collect::<Vec<_>>();
-    //println!("{:?}", parts);
+    if VERBOSE {
+        for (k, v) in flows.iter() {
+            print!("{}:\t", *k);
+            for &(i, greater, n, goto) in v.0.iter() {
+                print!("({} {} {:4} -> {:3})\t", i, match greater {true=>'>',false=>'<'}, n, goto);
+            }
+            println!(" -> {}", v.1);
+        }
+        println!("{:?}", parts);
+    }
     (flows, parts)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (flows, parts) = parse_game(input);
-    let mut sum: u32 = 0;
-    for part in parts {
+pub fn part_one(input: &str) -> Option<usize> {
+    let (flows, parts) = parse_input(input);
+
+    let sum = parts.iter().filter(|&&(x,m,a,s)|{
+        let vals = [x,m,a,s];
         let mut inflow = "in";
-        loop {
-            if inflow == "A" {
-                sum += part.iter().sum::<u32>();
-                // println!("ACCEPT {:?}", part);
-                break;
-            } else if inflow == "R" {
-                // println!("REJECT {:?}", part);
-                break;
-            }
-            let rules = flows.get(inflow).unwrap();
-            for rule in rules {
-                let (rule, goto) = rule;
-                if let Some((i, greater, n)) = rule {
-                    if (*greater && part[*i] > *n) || (!*greater && part[*i] < *n) {
-                        inflow = *goto;
-                        break;
-                    }
-                } else {
-                    inflow = *goto;
-                    break;
-                }
-            }
+        while inflow != "A" && inflow != "R" {
+            let (rules, goto) = flows.get(inflow).unwrap();
+            inflow = rules.iter()
+            .find(|&&(i, greater, n, _)| {
+                (greater && vals[i] > n) || (!greater && vals[i] < n)
+            }).map(|(_,_,_,goto)| *goto).unwrap_or(*goto);
         }
-    }
+        inflow == "A"
+    }).map(|&(x,m,a,s)| (x+m+a+s) as usize )
+    .sum();
+
     Some(sum)
 }
 
-fn eval_rule(inflow: &str, vals: &[usize; 4], rule: &(Option<(usize, bool, u32)>,&str)) -> [usize; 4] {
-    print!("rule {} -> ", inflow);
-    let mut vals = *vals;
-    let (rule, goto) = *rule;
-    if let Some((i, greater, n)) = rule {
-        if greater {
-            vals[i] = (4000 - n) as usize;
-            if goto == "A" {
-                println!("{:?} GT ACCEPTED", vals);
-                return vals;
-            } else if goto == "R" {
-                vals[i] = (n - 1) as usize;
-                println!("{:?} GT REJECTED", vals);
-                return vals;
-            }
-        } else {
-            vals[i] = (n - 1) as usize;
-            if goto == "A" {
-                println!("{:?} LT ACCEPTED", vals);
-                return vals;
-            } else if goto == "R" {
-                vals[i] = (4000 - n) as usize;
-                println!("{:?} LT REJECTED", vals);
-                return vals;
-            }
-        }
-    } else {
-        if goto == "A" {
-            println!("{:?} NN ACCEPTED", vals);
-            return vals;
-        } else if goto == "R" {
-            println!("{:?} NN REJECTED", vals);
-            return [0,0,0,0];
-        }
+fn count_accepted(flows: &FlowMap, inflow: &str, vals: [Vec<u32>; 4]) -> usize {
+    if inflow == "A" {
+        return vals.iter().map(|v| v.len()).product();
+    } else if inflow == "R" {
+        return 0;
     }
-    println!("{:?}", vals);
-    vals
-}
-
-fn eval_flow(inflow: &str, vals: &[usize; 4], accepted: &mut Vec<[usize; 4]>, flows: &HashMap<&str, Vec<(Option<(usize, bool, u32)>,&str)>>) {
-    //let accepted:Vec<[u32; 4]> = Vec::new();
-    print!("flow {} {:?}\n-> ", inflow, vals);
-    let mut new_vals = *vals;
-    let rules = flows.get(inflow).unwrap();
-    for rule in rules {
-        new_vals = eval_rule(inflow, &vals, rule);
-        if new_vals == [0,0,0,0] {
-            return;
-        }
-        let (_, goto) = rule;
-        if *goto == "A" {
-            println!("{:?} FLOW ACCEPTED", new_vals);
-            accepted.push(new_vals);
-        } else if *goto == "R" {
-            println!("{:?} FLOW REJECTED", new_vals);
-            continue;
-        } else {
-            eval_flow(goto, &new_vals, accepted, flows);
-        }
+    let mut vals = vals;
+    let mut accepted = 0;
+    let (flow, end) = flows.get(inflow).unwrap();
+    for &(i, greater, n, goto) in flow.iter() {
+        let mut newvals = vals.clone();
+        (newvals[i], vals[i]) = vals[i].iter().partition(|&val|
+            (greater && *val > n) || (!greater && *val < n));
+        accepted += count_accepted(flows, goto, newvals);
     }
-    println!("Accepted:");
-    for val in accepted.iter() {
-        println!("{:?}", val);
-    }
-    println!("");
+    accepted += count_accepted(flows, end, vals);
+    accepted
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let (flows, _) = parse_game(input);
-    for (k, v) in flows.iter() {
-        print!("{}:\t", *k);
-        for &(r, g) in v.iter() {
-            if r.is_some() {
-                let (i, greater, n) = r.unwrap();
-                print!("({} {} {:4} -> {:3})\t", i, match greater {true=>'>',false=>'<'}, n, g);
-            } else {
-                println!("(-> {})", g);
-            }
-        }
-        //println!("]");
-    }
-    let mut accepted:Vec<[usize; 4]> = Vec::new();
-
-    eval_flow("in", &[4000,4000,4000,4000], &mut accepted, &flows);
-    let mut total: usize = 0;
-    for val in accepted.iter() {
-        let sum = val.iter().product::<usize>() as usize;
-        total += sum;
-        println!("{:?} = {}", val, sum);
-    }
-    println!("=== {} ===", total);
-    println!("--- 167409079868000 ---");
-    println!("+/- {}", total - 167409079868000);
-
-    Some(total)
+    let (flows, _) = parse_input(input);
+    let valid = (1..=4000).map(|x| x as u32).collect::<Vec<_>>();
+    Some(count_accepted(&flows, "in", [valid.clone(),valid.clone(),valid.clone(),valid]))
 }
 
 #[cfg(test)]
